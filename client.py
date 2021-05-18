37a3804b9ea0709b0f56a0dbb80e5c37cc386c92
Saket <mail.saketbansal@gmail.com>
2021-05-18 21:39:40 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,33 +1,98 @@
 import socket
 import threading
+import tkinter
+import tkinter.scrolledtext
+from tkinter import simpledialog
 
-nickname = input("Choose a nickname: ")
+HOST = '127.0.0.1'
+PORT = 65535
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 65535))
+class Client:
 
-def receive():
-    while True:
-        try:
-            message = client.recv(1024).decode('ascii')
-            
-            if message == "NICK":
-                client.send(nickname.encode('ascii'))
-            else:
-                print(message)
+    def __init__(self, host, port):
+        
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host, port))
 
-        except:
-            print("An error occured!")
-            client.close()
-            break
+        msg = tkinter.Tk()
+        msg.withdraw()
 
-def write():
-    while True:
-        message = f"{nickname}: {input('')}"
-        client.send(message.encode('ascii'))
+        self.nickname = simpledialog.askstring("Nickname", "Please choose a nickname", parent=msg)
 
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
+        self.gui_done = False
+        self.running = True
 
-write_thread = threading.Thread(target=write)
-write_thread.start()
+        gui_thread = threading.Thread(target=self.gui_loop)
+        receive_thread = threading.Thread(target=self.receive)
+
+        gui_thread.start()
+        receive_thread.start()
+
+    def gui_loop(self):
+        
+        self.win = tkinter.Tk()
+        self.win.configure(bg="#c0d1d0")
+
+        self.chat_label = tkinter.Label(self.win, text="Chat:", bg="#c0d1d0")
+        self.chat_label.config(font=("BookAntiqua", 12))
+        self.chat_label.pack(padx=20, pady=5)
+
+        self.text_area = tkinter.scrolledtext.ScrolledText(self.win)
+        self.text_area.pack(padx=20, pady=5)
+        self.text_area.config(state='disabled')
+
+        self.msg_label = tkinter.Label(self.win, text="Message:", bg="#c0d1d0")
+        self.msg_label.config(font=("BookAntiqua", 12))
+        self.msg_label.pack(padx=20, pady=5)
+
+        self.input_area = tkinter.Text(self.win, height=3)
+        self.input_area.pack(padx=20, pady=5)
+
+        self.send_button = tkinter.Button(self.win, text='Send', command=self.write)
+        self.send_button.config(font=("BookAntiqua", 12))
+        self.send_button.pack(padx=20, pady=5)
+
+        self.gui_done = True
+
+        self.win.protocol("WM_DELETE_WINDOW", self.stop)
+
+        self.win.mainloop()
+        
+
+    def write(self):
+        msg = f"{self.nickname}: {self.input_area.get('1.0', 'end')}"
+        self.sock.send(msg.encode('ascii'))
+        self.input_area.delete('1.0', 'end')
+
+    def stop(self):
+        self.running = False
+        self.win.destroy()
+        self.sock.close()
+        exit(0)
+
+    def receive(self):
+        while self.running:
+           
+            try:
+                message = self.sock.recv(1024)
+                
+                if message == 'NICK':
+                    self.sock.send(self.nickname.encode('ascii'))
+                
+                else:
+                   
+                    if self.gui_done:
+                        self.text_area.config(state='normal')
+                        self.text_area.insert('end', message)
+                        self.text_area.yview('end')
+                        self.text_area.config(state='disabled')
+
+            except ConnectionAbortedError:
+                break
+
+            except:
+                print("Error")
+                self.sock.close()
+                break
+
+client = Client(HOST, PORT)         
